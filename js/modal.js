1142da19d09a55138b3827879afb9a1aa172834b
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-16 18:10:21 +0300

    add EventListener
    додав в модалку слухача на beckdrop та keyboard

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,33 @@
-(() => {
+
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    backdrop : document.querySelector(".backdrop")
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.backdrop.addEventListener("click", closeModalByBackDrop)
 
-  function toggleModal() {
+  function openModal() {
     refs.modal.classList.toggle("is-hidden");
+    document.body.addEventListener("keydown", closeModalByKeyboard);
   }
-})();
+  function closeModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.removeEventListener("keydown", closeModalByKeyboard);
+  }
+
+function closeModalByKeyboard (event){
+  if(event.code === "Escape") {
+closeModal();
+}
+}
+
+function closeModalByBackDrop (event){
+  if(event.currentTarget === event.target){
+    closeModal()
+  }
+}
+
